405393b7e3a22bfb6bf14856a94e453dce0a1eba
andresys <andreees.ys@gmail.com>
2023-04-22 13:33:25 +0200

    practica5

---
+++ practica5_839756_842236/Rules.hs
@@ -0,0 +1,59 @@
+module Rules where
+
+-- Curva de Koch
+rulesKoch :: Char -> String
+rulesKoch c
+  | c == 'F' = "F+F--F+F"
+  | otherwise = [c]
+
+-- Curva de Koch cuadrada
+rulesSquaredKoch :: Char -> String
+rulesSquaredKoch c
+  | c == 'F' = "F+F-F-F+F"
+  | otherwise = [c]
+
+-- Copo de nieve de Koch
+rulesSnowflake :: Char -> String
+rulesSnowflake c
+  | c == 'F' = "F-F++F-F"
+  | otherwise = [c]
+
+-- Anticopo de nieve de Koch
+rulesAntiSnowflake :: Char -> String
+rulesAntiSnowflake c
+  | c == 'F' = "F+F--F+F"
+  | otherwise = [c]
+
+-- Isla de Minkowski
+rulesMinkowski :: Char -> String
+rulesMinkowski c
+  | c == 'F' = "F+F-F-FF+F+F-F"
+  | otherwise = [c]
+
+-- Triángulo de Sierpinsky
+rulesSierpinsky :: Char -> String
+rulesSierpinsky c
+  | c == 'F' = "F-G+F+G-F"
+  | c == 'G' = "GG"
+  | otherwise = [c]
+
+-- Sierpinsky Arrowhead
+rulesArrowhead :: Char -> String
+rulesArrowhead c
+  | c == 'F' = "G-F-G"
+  | c == 'G' = "F+G+F"
+  | otherwise = [c]
+
+-- Curva de Hilbert
+rulesHilbert :: Char -> String
+rulesHilbert c
+  | c == 'f' = "-g>+f>f+>g-"
+  | c == 'g' = "+f>-g>g->f+"
+  | otherwise = [c]
+
+-- Curva de Gosper
+rulesGosper :: Char -> String
+rulesGosper c
+  | c == 'F' = "F-G--G+F++FF+G-"
+  | c == 'G' = "+F-GG--G-F++F+G"
+  | otherwise = [c]
--- practica5_839756_842236/main.hs
+++ practica5_839756_842236/main.hs
@@ -1,14 +1,23 @@
-import Tplot
 import Lsystem
+import Rules
 import SVG
+import Tplot
+import Turtle
 
-rulesArrowhead :: Char -> String
-rulesArrowhead c
-    | c == 'F' = "G-F-G"
-    | c == 'G' = "F+G+F"
-    | otherwise = [c]
+testList =
+  [ ("koch", rulesKoch, "F", 5, 1, 60, 0),
+    ("squaredkoch", rulesSquaredKoch, "F", 5, 1, 90, 0),
+    ("snowflake", rulesSnowflake, "F++F++F", 5, 1, 60, 90),
+    ("antisnowflake", rulesAntiSnowflake, "F++F++F", 5, 1, 60, 180),
+    ("minkowski", rulesMinkowski, "F+F+F+F", 5, 1, 90, 90),
+    ("sierpinsky", rulesSierpinsky, "F-G-G", 5, 1, 120, 180),
+    ("arrowhead", rulesArrowhead, "F", 5, 1, 60, 180),
+    ("hilbert", rulesHilbert, "f", 5, 1, 90, 90),
+    ("gosper", rulesGosper, "F", 3, 1, 60, 90)
+  ]
 
---main = print ( translate ( lsystem rulesArrowhead "F" 1 ) )
---main = savesvg "arrowhead" (tplot (1, 90, (0, 0), 90) lsystem rulesArrowhead "F" 30)
+test :: (String, Char -> String, String, Int, Float, Float, Heading) -> IO ()
+test (name, rule, axiom, depth, step, turn, heading) = savesvg name (tplot (step, turn, (0, 0), heading) (translate (lsystem rule axiom depth)))
 
-main = savesvg "arrowhead" (tplot (1, 60, (0, 0), 90) ( translate ( lsystem rulesArrowhead "F" 5 ) ) )
+-- main = savesvg "arrowhead" (tplot (1, 60, (0, 0), 90) (translate (lsystem rulesArrowhead "F" 5)))
+main = mapM test testList
